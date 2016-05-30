1eb25e9df97d6ee0f812ce3eaf2eeb92f5d2df00
Ines Müller <im984935@fh-muenster.de>
2016-05-30 12:33:19 +0200

    CreateEventFragment: Methode für Event erstellen/speichern hinzugefügt.

--- app/src/main/java/com/misd/cookapp/CreateEventFragment.java
+++ app/src/main/java/com/misd/cookapp/CreateEventFragment.java
@@ -4,16 +4,25 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+
+import java.util.Calendar;
+
+import static com.misd.cookapp.HelperMethods.pasteCalendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +32,7 @@ import android.widget.Toast;
  */
 public class CreateEventFragment extends Fragment {
     private static final String TAG = CreateEventFragment.class.toString();
+    public static final String ARGS_EVENT_OBJECT = "args_event_object";
 
     private OnFragmentInteractionListener mListener;
 
@@ -40,7 +50,7 @@ public class CreateEventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.create_event_fragment, container, false);
-        registerSafeEventListener();
+        registerSafeEventListener(rootView);
         return rootView;
     }
 
@@ -78,39 +88,77 @@ public class CreateEventFragment extends Fragment {
 /**
  * TODO Event erstellen - Übergaben speichern
  */
-    private void registerSafeEventListener() {
-    Button safeEvent = (Button) getActivity().findViewById(R.id.eventSafe);
+    private void registerSafeEventListener(View rootView) {
+    Button safeEvent = (Button) rootView.findViewById(R.id.eventSafe);
     safeEvent.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            EditText eventMealName = (EditText) getActivity().findViewById(R.id.editText);
-            eventMealName.getText();
+            EditText textEventMealName = (EditText) getActivity().findViewById(R.id.editText);
+            String eventMealName = textEventMealName.getText().toString();
 
-            EditText eventDescription = (EditText) getActivity().findViewById(R.id.eventDescription);
-            eventDescription.getText();
+            EditText textEventDescription = (EditText) getActivity().findViewById(R.id.eventDescription);
+            String eventDescription = textEventDescription.getText().toString();
 
-            // Heimatadresse verwenden
+            // TODO Heimatadresse verwenden
 
-            EditText eventStreet = (EditText) getActivity().findViewById(R.id.eventStreet);
-            eventStreet.getText();
+            EditText textEventStreet = (EditText) getActivity().findViewById(R.id.eventStreet);
+            String eventStreet = textEventStreet.getText().toString();
 
-            EditText eventPostalCode = (EditText) getActivity().findViewById(R.id.eventPostalCode);
+            EditText textEventPostalCode = (EditText) getActivity().findViewById(R.id.eventPostalCode);
+            int eventPostalCode = Integer.parseInt(textEventPostalCode.getText().toString());
 
-            EditText eventCity = (EditText) getActivity().findViewById(R.id.eventCity);
+            EditText textEventCity = (EditText) getActivity().findViewById(R.id.eventCity);
+            String eventCity = textEventCity.getText().toString();
 
             RadioGroup radioGroupGender = (RadioGroup) getActivity().findViewById(R.id.radioGroupGender);
             int selectedId = radioGroupGender.getCheckedRadioButtonId(); // get selected radio button from radioGroup
-            RadioButton radioGenderButton = (RadioButton) getActivity().findViewById(selectedId); // find the radiobutton by returned id
-            radioGenderButton.getText();
+            // Check which radiobutton was clicked
+            char genderRestriction;
+            switch(selectedId) {
+                case R.id.radiobutton_onlyMale:
+                    genderRestriction = 'm';
+                    break;
+                case R.id.radiobutton_onlyFemale:
+                    genderRestriction = 'w';
+                    break;
+                default:
+                    genderRestriction = 'b';
+            }
 
-            // Unverträglichkeiten
-            // Altersbeschränkung
-            // User
-            // Calendar
+            CheckBox chkEventLactoseFree = (CheckBox) getActivity().findViewById(R.id.lactoseFree);
+            boolean eventLactoseFree = chkEventLactoseFree.isChecked();
+            CheckBox chkEventGlutenFree = (CheckBox) getActivity().findViewById(R.id.glutenFree);
+            boolean eventGlutenFree = chkEventGlutenFree.isChecked();
+            CheckBox chkEventFructoseFree = (CheckBox) getActivity().findViewById(R.id.fructoseFree);
+            boolean eventFructoseFree = chkEventFructoseFree.isChecked();
+            CheckBox chkEventSorbitFree = (CheckBox) getActivity().findViewById(R.id.sorbitFree);
+            boolean eventSorbitFree = chkEventSorbitFree.isChecked();
 
-        // erwartet: String eventDescription, Meal eventMeal, int eventMinAge, int eventMaxAge, char eventGender, String eventStreet, int eventPostal, String eventCity, User eventOwner, Calendar eventDate
-        // Event createdEvent = new Event(eventDescription,(new Meal(eventMealName,??, ??, ??, ??)),?,?, radioGenderButton, eventStreet, eventPostalCode, eventCity,(new User()),?);
+            RangeSeekBar textEventAge = (RangeSeekBar) getActivity().findViewById(R.id.eventAge);
+            int eventMinAge = textEventAge.getAbsoluteMinValue().intValue();
+            int eventMaxAge = textEventAge.getAbsoluteMaxValue().intValue();
+
+            CalendarView calEventDateTime = (CalendarView) getActivity().findViewById(R.id.eventDateTime);
+
+            /*
+             * TODO DummyUser sowie Calendar ersetzen
+             */
+
+            Event createdEvent = new Event(eventDescription,(new Meal(eventMealName,eventLactoseFree, eventGlutenFree, eventFructoseFree, eventSorbitFree)),eventMinAge,eventMaxAge, genderRestriction, eventStreet, eventPostalCode, eventCity,(new User("Mustermann", "Max", "Teststraße", 48249, "Dülmen", 'm', 18, "+49123456")),pasteCalendar(2010,10,2,12,34));
+
+            //Change to the ShowEventFragment
+            Fragment fragment = new ShowEventFragment();
+            Bundle args = new Bundle();
+            args.putSerializable(ARGS_EVENT_OBJECT, createdEvent);
+            fragment.setArguments(args);
+
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
         }
     });
   }
+
 }
